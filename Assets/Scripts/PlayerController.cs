cf3e622d93d7ef60ba69b7312ca0075fdc7fdb16
Ron Welch <mmcelt@yahoo.com>
2020-04-11 17:03:08 -0600

    7 - Moving the Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{	
+{
 	#region Fields
 
-	
+	[SerializeField] float _moveSpeed;
+
+	Vector2 _moveInput;
+
 	#endregion
 
 	#region MonoBehaviour Methods
@@ -18,7 +21,10 @@ public class PlayerController : MonoBehaviour
 	
 	void Update() 
 	{
-		
+		_moveInput.x = Input.GetAxisRaw("Horizontal") * _moveSpeed * Time.deltaTime;
+		_moveInput.y = Input.GetAxisRaw("Vertical") * _moveSpeed * Time.deltaTime;
+
+		transform.position += new Vector3(_moveInput.x, _moveInput.y, 0f);
 	}
 	#endregion
 
