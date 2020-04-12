972d18bcffb46d7b76f9e44f1824992b1563b2a8
Ron Welch <mmcelt@yahoo.com>
2020-04-11 23:01:08 -0600

    SECTION 4 - SHOOTING: 13 - Making Bullets

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] float _speed = 7.5f;
+	[SerializeField] Rigidbody2D _theRB;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		_theRB.velocity = transform.right * _speed;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Destroy(gameObject);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
