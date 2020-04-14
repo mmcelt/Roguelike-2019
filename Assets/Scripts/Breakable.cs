795d232370941d53a2505f7be223c860a85f1610
Ron Welch <mmcelt@yahoo.com>
2020-04-14 10:55:37 -0600

    36 - Adding Broken Pieces

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Breakable : MonoBehaviour
-{	
+{
 	#region Fields
 
-	
+	[SerializeField] GameObject[] _brokenPieces;
+	[SerializeField] int _maxPieces = 5;
+
 	#endregion
 
 	#region MonoBehaviour Methods
 
 	void Start() 
-	{
-		
-	}
-	
-	void Update() 
 	{
 		
 	}
@@ -26,6 +23,13 @@ public class Breakable : MonoBehaviour
 		if (other.CompareTag("Player") && PlayerController.Instance.DashCounter > 0)
 		{
 			Destroy(gameObject);
+
+			int piecesToDrop = Random.Range(1, _maxPieces + 1);
+			for (int i=0; i<piecesToDrop; i++)
+			{
+				int randomPiece = Random.Range(0, _brokenPieces.Length);
+				Instantiate(_brokenPieces[randomPiece], transform.position, transform.rotation);
+			}
 		}
 	}
 	#endregion
