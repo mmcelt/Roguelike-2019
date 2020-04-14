2e62bfaab439fe863d269dd13c2bbd48626ce2fd
Ron Welch <mmcelt@yahoo.com>
2020-04-14 12:12:03 -0600

    39 - Automatically Assigning Sort Order

---
+++ Assets/Scripts/SpriteSortOrder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSortOrder : MonoBehaviour
+{
+	#region Fields
+
+	SpriteRenderer _theSprite;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		_theSprite = GetComponent<SpriteRenderer>();
+		_theSprite.sortingOrder = Mathf.RoundToInt(transform.position.y * -10f);
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
