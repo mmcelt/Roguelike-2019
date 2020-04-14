b2461dd7211654f9d4e76029c5ef8fa60ec7d29b
Ron Welch <mmcelt@yahoo.com>
2020-04-14 08:13:43 -0600

    31 - Adding Static Dangers

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{	
+	#region Fields
+
+	
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+			PlayerHealthController.Instance.DamagePlayer();
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+			PlayerHealthController.Instance.DamagePlayer();
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+			PlayerHealthController.Instance.DamagePlayer();
+	}
+
+	void OnCollisionStay2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+			PlayerHealthController.Instance.DamagePlayer();
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
