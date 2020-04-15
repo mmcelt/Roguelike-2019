1c9c9d26a6a350cfeb4a5939a947939996b653c5
Ron Welch <mmcelt@yahoo.com>
2020-04-15 09:49:31 -0600

    49 - Creating the Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	#region Fields
+
+	public static CameraController Instance;
+
+	[SerializeField] float _moveSpeed;
+	[SerializeField] Transform _target;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		if (_target == null) return;
+
+		transform.position = Vector3.MoveTowards(transform.position, new Vector3(_target.position.x, _target.position.y, transform.position.z), _moveSpeed * Time.deltaTime);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
