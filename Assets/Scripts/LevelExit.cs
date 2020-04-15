6638b1574c97f5f9cd3086b515d5730eb0c9d78d
Ron Welch <mmcelt@yahoo.com>
2020-04-15 15:58:18 -0600

    SECTION 11 - LEVEL FLOW: 54 - Completing a Level

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] string _levelToLoad;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			SceneManager.LoadScene(_levelToLoad);
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
