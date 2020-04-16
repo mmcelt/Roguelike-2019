b5125033186142d5e894d13022b040138acfa60f
Ron Welch <mmcelt@yahoo.com>
2020-04-16 11:24:48 -0600

    SECTION 12 - MENUS: 58 - Creating Title Screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] string _levelToLoad;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene(_levelToLoad);
+	}
+
+	public void ExitGame()
+	{
+#if UNITY_EDITOR
+
+		UnityEditor.EditorApplication.isPlaying = false;
+
+#endif
+		Application.Quit();
+	}
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
