86a9b2aac02455ef84f31e4f69b48aa0faad79ab
Ron Welch <mmcelt@yahoo.com>
2020-04-16 12:41:31 -0600

    59 - Creating Victory Screen

---
+++ Assets/Scripts/VictoryScreen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryScreen : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] GameObject _anyKeyText;
+	[SerializeField] string _mainMenuScene;
+	[SerializeField] float _waitForAnyKey = 2f;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		StartCoroutine(VictorySequencer());
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+	IEnumerator VictorySequencer()
+	{
+		yield return new WaitForSeconds(_waitForAnyKey);
+
+		_anyKeyText.SetActive(true);
+
+		yield return new WaitUntil(() => Input.anyKeyDown);
+
+		SceneManager.LoadScene(_mainMenuScene);
+	}
+	#endregion
+}
