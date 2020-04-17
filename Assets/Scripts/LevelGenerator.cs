e4436315b616631f17256c6974ba60e7bc520547
Ron Welch <mmcelt@yahoo.com>
2020-04-16 21:01:28 -0600

    SECTION 13 - GENERATING PROCEDURAL LEVELS: 63/64 - Creating the Start Room

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] GameObject _layoutRoom;
+	[SerializeField] int _distanceToEnd;
+	[SerializeField] Color _startColor, _endColor;
+	[SerializeField] Transform _generationPoint;
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		//generate the starting room...
+		Instantiate(_layoutRoom, _generationPoint.position, _generationPoint.rotation).GetComponent<SpriteRenderer>().color = _startColor;
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
